c058a7494df12f64dea84ff5920343de85fab695
eetpang <eetpang@gmail.com>
2017-03-25 03:11:55 -0400

    bower/lib

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var app = express();
 
 var port = 5000;
 
-app.listen(5000, function(err){
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/',function(req,res){
+    res.send('hello world');
+});
+
+app.listen(port, function(err){
     console.log('running server on port'+ port);
 });
